98f3e3361c042737e8d05febc348a65481b284eb
AJITH P MOHAN <ajithpmohan90@gmail.com>
2020-03-14 21:40:30 +0530

    Link Social Logins with Firebase in React

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -25,6 +25,16 @@ const INITIAL_STATE = {
   error: null,
 };
 
+const ERROR_CODE_ACCOUNT_EXISTS =
+  'auth/account-exists-with-different-credential';
+
+const ERROR_MSG_ACCOUNT_EXISTS = `
+  An account with an E-Mail address to
+  this social account already exists. Try to login from
+  this account instead and associate your social accounts on
+  your personal account page.
+`;
+
 class SignInFormBase extends Component {
   constructor(props) {
     super(props);
@@ -103,8 +113,13 @@ class SignInGoogleBase extends Component {
         this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
+
         this.setState({ error });
       });
+
     event.preventDefault();
   };
 
@@ -146,6 +161,10 @@ class SignInFacebookBase extends Component {
         this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
+
         this.setState({ error });
       });
 
@@ -191,6 +210,10 @@ class SignInTwitterBase extends Component {
         this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
+
         this.setState({ error });
       });
 
